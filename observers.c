e37b8a19c605b26bf7bda3672aa2a512c2d8cb40
Fabrizio Gennari <fabrizio@work>
2011-09-27 14:00:28 +0200

    Add get_observers()

--- observers.c
+++ observers.c
@@ -1,5 +1,6 @@
 #include "observers.h"
 #include "wav2prg_api.h"
+//#include "wav2prg_types.h"
 
 #include <stdlib.h>
 #include <string.h>
@@ -21,6 +22,31 @@ static void add_observer_to_observed(struct wav2prg_observed *observed, const ch
   observed->observers[number_of_observers + 1].recognize_func = NULL;
 }
 
+static struct wav2prg_observed* get_observed(const char *observed_name, enum wav2prg_bool add_if_missing){
+  struct wav2prg_observed *current_observed;
+  int number_of_observed = 0;
+
+  if (observed_list == NULL)
+  {
+    observed_list = calloc(sizeof(struct wav2prg_observed), 1);
+  }
+  for(current_observed = observed_list; current_observed->name != NULL; current_observed++, number_of_observed++){
+    if (!strcmp(observed_name, current_observed->name))
+      return current_observed;
+  }
+  if (add_if_missing)
+  {
+    observed_list = realloc(observed_list, sizeof(observed_list[0]) * (2 + number_of_observed));
+    current_observed = &observed_list[number_of_observed];
+    current_observed->name = observed_name;
+    current_observed->observers = calloc(sizeof(struct wav2prg_observed_loaders), 1);
+    current_observed[1].name = NULL;
+    current_observed[1].observers = NULL;
+    return current_observed;
+  }
+  return NULL;
+}
+
 static void add_one_observed(const char *observer_name, const char *observed_name, wav2prg_recognize_block recognize_func){
   if (!strcmp(observed_name, "khc"))
   {
@@ -33,26 +59,7 @@ static void add_one_observed(const char *observer_name, const char *observed_nam
     add_one_observed(observer_name, "Kernal data chunk 2nd copy", recognize_func);
   }
   else{
-    struct wav2prg_observed *current_observed;
-    int number_of_observed = 0;
-
-    if (observed_list == NULL)
-    {
-      observed_list = calloc(sizeof(struct wav2prg_observed), 1);
-    }
-    for(current_observed = observed_list; current_observed->name != NULL; current_observed++, number_of_observed++){
-      if (!strcmp(observed_name, current_observed->name))
-        break;
-    }
-    if (current_observed->name == NULL)
-    {
-      observed_list = realloc(observed_list, sizeof(observed_list[0]) * (2 + number_of_observed));
-      current_observed = &observed_list[number_of_observed];
-      current_observed->name = observed_name;
-      current_observed->observers = calloc(sizeof(struct wav2prg_observed_loaders), 1);
-      current_observed[1].name = NULL;
-      current_observed[1].observers = NULL;
-    }
+    struct wav2prg_observed *current_observed = get_observed(observed_name, wav2prg_true);
     add_observer_to_observed(current_observed, observer_name, recognize_func);
   }
 }
@@ -62,4 +69,9 @@ void add_observed(const char *observer_name, const struct wav2prg_observed_loade
 
   for(current_observed = observed; current_observed->loader != NULL; current_observed++)
     add_one_observed(observer_name, current_observed->loader, current_observed->recognize_func);
+}
+
+struct wav2prg_observed_loaders* get_observers(const char *observed_name){
+  struct wav2prg_observed *current_observed = get_observed(observed_name, wav2prg_false);
+  return current_observed ? current_observed->observers : NULL;
 }
