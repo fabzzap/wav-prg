992323126658e8290965d64f5084c72018a89039
Fabrizio Gennari <fabrizio@home>
2012-08-25 11:50:25 +0200

    Add prototypes

--- loaders.h
+++ loaders.h
@@ -4,3 +4,6 @@ char** get_loaders(void);
 void wav2prg_set_plugin_dir(const char *name);
 void wav2prg_set_default_plugin_dir(void);
 const char* wav2prg_get_plugin_dir(void);
+void cleanup_modules_used_by_loaders(void);
+void cleanup_modules_used_by_observers(void);
+void cleanup_loaders_and_observers(void);
